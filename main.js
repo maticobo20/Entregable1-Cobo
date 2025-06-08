a0cae41c537455e9a239612b91ceb6b8e2670fcc
Matias Cobo <maticobo20@gmail.com>
2025-06-08 15:51:28 -0300

    piedra, papel o tijera

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const opciones = ["Piedra", "Papel", "Tijera"];
-
+const MAX_RONDAS = 3;
 
 let victoriasJugador1 = 0;
 let victoriasJugador2 = 0;
 let empateTotal = 0;
+let rondasJugadas = 0;
 
 let historial = [];
 
@@ -15,6 +16,13 @@ function generarEleccionJugador2() {
 }
 
 function jugar(eleccionJugador1) {
+    if (rondasJugadas >= MAX_RONDAS) {
+        alert("¡Se alcanzaron las 3 rondas! El juego se reiniciará.");
+        reiniciarJuego();
+        return;
+    }
+
+
     console.log(`Jugador eligió: ${eleccionJugador1}`);
     const eleccionJugador2 = generarEleccionJugador2();
     let resultado = "";
@@ -37,6 +45,7 @@ function jugar(eleccionJugador1) {
         console.log(`Perdiste.`);
     }
 
+    rondasJugadas++;
 
     historial.push({
         jugador1: eleccionJugador1,
@@ -45,6 +54,24 @@ function jugar(eleccionJugador1) {
     });
 
     actualizarHistorial();
+
+    if (rondasJugadas >= MAX_RONDAS) {
+        setTimeout(() => {
+            let mensajeFinal = `Juego terminado. Jugador 1: ${victoriasJugador1} victorias. Jugador2: ${victoriasJugador2} victorias. Empates: ${empateTotal}`;
+            
+            if (victoriasJugador1 > victoriasJugador2) {
+                mensajeFinal += "¡Jugador1 gana la partida!";
+            } else if (victoriasJugador2 > victoriasJugador1) {
+                mensajeFinal += "¡Jugador2 gana la partida!";
+            } else {
+                mensajeFinal += "La partida terminó en empate";
+            }
+
+
+            alert(mensajeFinal);
+            reiniciarJuego();
+        }, 100);
+    }
 }
 
 function reiniciarJuego() {
@@ -52,6 +79,7 @@ function reiniciarJuego() {
     victoriasJugador2 = 0;
     empateTotal = 0;
     historial = [];
+    rondasJugadas = 0;
 
     console.log(`Juego reiniciado.`);
 
